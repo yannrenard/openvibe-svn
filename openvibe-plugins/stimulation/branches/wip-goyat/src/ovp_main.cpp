98781dcc776605f8b10984b02551c74457e2c52f
gipsa-lab <gipsa-lab@c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f>
2010-03-22 16:09:39 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe/trunk@1937 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

---
+++ openvibe-plugins/stimulation/branches/wip-goyat/src/ovp_main.cpp
@@ -0,0 +1,35 @@
+#include "ovp_defines.h"
+
+#include "ovpCXMLStimulationScenarioPlayer.h"
+#include "ovpCKeyboardStimulator.h"
+
+#include "box-algorithms/stimulation/ovpCBoxAlgorithmStimulationMultiplexer.h"
+#include "box-algorithms/stimulation/ovpCBoxAlgorithmSoundPlayer.h"
+
+#include "box-algorithms/stimulation/ovpCBoxAlgorithmRunCommand.h"
+#include "box-algorithms/stimulation/ovpCBoxAlgorithmLuaStimulator.h"
+
+#include "box-algorithms/stimulation/adaptation/ovpCBoxAlgorithmStimulationFilter.h"
+
+#include "ovpCBoxAlgorithmP300SpellerStimulator.h"
+#include "ovpCBoxAlgorithmP300IdentifierStimulator.h"
+
+OVP_Declare_Begin();
+	rPluginModuleContext.getTypeManager().registerEnumerationType(OVP_TypeId_StimulationFilterAction, "Stimulation Filter Action");
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_StimulationFilterAction, "Select", OVP_TypeId_StimulationFilterAction_Select.toUInteger());
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_StimulationFilterAction, "Reject", OVP_TypeId_StimulationFilterAction_Reject.toUInteger());
+
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CXMLStimulationScenarioPlayerDesc);
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CKeyboardStimulatorDesc);
+
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmStimulationMultiplexerDesc);
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmSoundPlayerDesc);
+
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmRunCommandDesc);
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmLuaStimulatorDesc);
+
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmStimulationFilterDesc);
+
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmP300SpellerStimulatorDesc);
+	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmP300IdentifierStimulatorDesc);
+OVP_Declare_End();
