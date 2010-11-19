7d2b6f3adc95b0e373fc3325a2ecbbb6e38a2362
Gelu Ionescu <gelu.ionescu@gipsa-lab.grenoble-inp.fr>
2010-11-19 11:46:32 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe/trunk@2542 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- openvibe-applications/acquisition-server/branches/wip-gionescu/src/gipsalab-mitsarEEG202A/ovasCAcqServerMitsarEEG202ADataInputStream.cpp
+++ openvibe-applications/acquisition-server/branches/wip-gionescu/src/gipsalab-mitsarEEG202A/ovasCAcqServerMitsarEEG202ADataInputStream.cpp
@@ -1,6 +1,8 @@
 #define INCUDE_WINDOWS
 #include "ovasCAcqServerMitsarEEG202ADataInputStream.h"
 
+#include "iostream"
+
 using namespace OpenViBEAcquisitionServer;
 using namespace OpenViBE;
 
@@ -24,6 +26,8 @@ OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::open()
 {
 	m_hInstance			= ::LoadLibrary(m_strDllFileName.c_str());
 
+	std::cout << "GELU start CAcqServerMitsarEEG202ADataInputStream::open()" << std::endl;
+	
 	if(!m_hInstance)
 		return false;
 
@@ -44,12 +48,16 @@ OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::open()
 		
 		return false;
 	}
+	
+	std::cout << "GELU OK CAcqServerMitsarEEG202ADataInputStream::open()" << std::endl;
 
 	return true;
 }
 
 OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::close()
 {
+	std::cout << "GELU start CAcqServerMitsarEEG202ADataInputStream::close()" << std::endl;
+	
 	m_fpUninitialize();
 
 	::FreeLibrary(m_hInstance);
@@ -60,26 +68,49 @@ OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::close()
 	m_fpStop			= 0;
 	m_fpUninitialize	= 0;
 	m_fpLoop			= 0;
-	
+
+	std::cout << "GELU OK CAcqServerMitsarEEG202ADataInputStream::close()" << std::endl;
+
 	return true;
 }
 
 OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::readInfo()
 {
+	std::cout << "GELU start CAcqServerMitsarEEG202ADataInputStream::readInfo()" << std::endl;
+	std::cout << "GELU OK CAcqServerMitsarEEG202ADataInputStream::readInfo()" << std::endl;
+	
 	return true;
 }
 
 OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::read()
 {
-	return m_fpLoop((OpenViBE::float32*) getBuffer()) == 0;
+	std::cout << "GELU start CAcqServerMitsarEEG202ADataInputStream::read()" << std::endl;
+	
+	OpenViBE::int32 ret = m_fpLoop((OpenViBE::float32*) getBuffer());
+	
+	std::cout << "GELU " <<  ((ret == 0) ? "OK" : "FAIL") << " CAcqServerMitsarEEG202ADataInputStream::read()" << std::endl;
+
+	return ret == 0;
 }
 
 OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::start()
 {
-	return m_fpStart(m_ui32RefIndex) == 0;
+	std::cout << "GELU start CAcqServerMitsarEEG202ADataInputStream::start()" << std::endl;
+	
+	OpenViBE::int32 ret = m_fpStart(m_ui32RefIndex);
+	
+	std::cout << "GELU " <<  int(m_ui32RefIndex) << ((ret == 0) ? " OK" : " FAIL") << " CAcqServerMitsarEEG202ADataInputStream::start()" << std::endl;
+
+	return ret == 0;
 }
 
 OpenViBE::boolean CAcqServerMitsarEEG202ADataInputStream::stop()
 {
-	return m_fpStop() == 0;
+	std::cout << "GELU start CAcqServerMitsarEEG202ADataInputStream::stop()" << std::endl;
+	
+	OpenViBE::int32 ret = m_fpStop();
+	
+	std::cout << "GELU " <<  ((ret == 0) ? "OK" : "FAIL") << " CAcqServerMitsarEEG202ADataInputStream::stop()" << std::endl;
+
+	return ret == 0;
 }
