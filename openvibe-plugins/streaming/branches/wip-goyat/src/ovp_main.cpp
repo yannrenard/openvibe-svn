30378a2875d6f57e7cc7442d0073667d5f7b6592
gipsa-lab <gipsa-lab@c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f>
2011-04-19 12:31:01 +0000

    openvibe-plugins-streaming
    +ovpCBoxAlgorithmStreamedMatrixMerger.h/.cpp : add a box to concatenate data along a dimension of the matrix. Is not perfect yet, work actually only for specific matrix and dimension
    
    git-svn-id: svn://scm.gforge.inria.fr/svn/openvibe/trunk@2938 c330d7e9-fc0c-0410-a5b3-fd85c6f5aa8f

--- openvibe-plugins/streaming/branches/wip-goyat/src/ovp_main.cpp
+++ openvibe-plugins/streaming/branches/wip-goyat/src/ovp_main.cpp
@@ -2,10 +2,12 @@
 
 #include "box-algorithms/streaming/ovpCBoxAlgorithmStreamedMatrixMultiplexer.h"
 #include "box-algorithms/streaming/ovpCBoxAlgorithmSignalMerger.h"
+#include "box-algorithms/streaming/ovpCBoxAlgorithmStreamedMatrixMerger.h"
 
 #include <vector>
 
 OVP_Declare_Begin()
 	OVP_Declare_New(OpenViBEPlugins::Streaming::CBoxAlgorithmStreamedMatrixMultiplexerDesc);
 	OVP_Declare_New(OpenViBEPlugins::Streaming::CBoxAlgorithmSignalMergerDesc)
+	OVP_Declare_New(OpenViBEPlugins::Streaming::CBoxAlgorithmStreamedMatrixMergerDesc)
 OVP_Declare_End()
